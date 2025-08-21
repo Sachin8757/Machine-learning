c24252f67c8384349fa27b4df46b56bedb47c588
priyanshukumar72 <priyanshu727989@gmail.com>
2025-08-21 13:57:06 +0530

    day 5

---
+++ python/class_5/dictionary.py
@@ -0,0 +1,27 @@
+#dictionary is a collection of key_value pairs.
+#syntax
+marks={
+    "sachin":100,
+    "mohan":69,
+    "rohan":90
+}
+print(marks)
+
+# marks.update({"sachin":101})#update marks of sachin
+# print("Sachin's marks " + str(marks.get("sachin"))) # To get a value given by user
+
+# marks.clear() # To clear marks dictionary
+
+# print(marks)
+
+# marks.popitem() # To pop element from marks dic
+# marks.popitem() # To pop element from marks dic
+# print(marks)
+
+# marks.pop("mohan")  # Removes the element with key 'mohan' from the dictionary
+# print(marks)
+
+# hello={}
+# hello = marks.copy() # To copy element from marks to hello
+# print(hello)
+
